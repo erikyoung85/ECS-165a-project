bb2faf51d0e86d3210498c458c42f46bf0809d2a
aliouziz <88304905+aliouziz@users.noreply.github.com>
2022-02-24 22:08:04 -0800

    merge

--- lstore/table.py
+++ lstore/table.py
@@ -1,6 +1,7 @@
 from lstore.index import Index
 from lstore.page import Page
-from time import time
+import time, threading
+from datetime import datetime
 
 INDIRECTION_COLUMN = 0
 RID_COLUMN = 1
@@ -37,6 +38,7 @@ class Table:
         self.pagerange_capacity = 16
         self.basepage(pagerange_idx=0)
         self.db = None
+        threading.Timer(30, self.__merge()).start()
         # keep track of total records to create the next rid
         self.rid_counter = 1
 
@@ -88,6 +90,62 @@ class Table:
             pagerange.tail_page_idxs.append(pagerange.pages - 1)
 
     def __merge(self):
-        print("merge is happening")
-        pass
+        page = Page()
+        page.path = self.name + " page_range " + str(len(self.pagerange))
+        self.pagerange.append(page)
+        pagerange_amt = len(self.pagerange)
+
+
+    # Iterates through every page range
+        for ranges in range(pagerange_amt):
+            pagerange = self.pagerange[ranges]
+
+        # checks if the page range has been merged and if it has 0 tail pages
+            if not pagerange.hasMerged and len(pagerange.tail_page_idxs) > 0:
+                pagerange.hasMerged = True
+
+                for col in self.num_columns + 4:
+                    for basepage in self.page.base_page_idxs:
+                        pagerange.append(self.pagerange[ranges][col][basepage])
+
+                for pages in pagerange[INDIRECTION_COLUMN]:
+                    for values in pagerange[INDIRECTION_COLUMN][pages]:
+                        if values[0].to_bytes(8, 'big') != page:
+                            (page_range_idx, page_idx, offset) = pagerange[INDIRECTION_COLUMN][pages][values]
+
+                            page_idx_latest = int.from_bytes(
+                                pagerange.array[INDIRECTION_COLUMN][pages][offset: offset + 4], 'big')
+                            byte_offset_latest = int.from_bytes(pagerange.array[INDIRECTION_COLUMN][pages][offset + 4: offset + 8], 'big')
+
+                            # if the last base page is full, make another one
+                            use_page_idx = pagerange.base_page_idxs[-1]
+                            if not pagerange.has_capacity(use_page_idx):
+                                self.table.new_pages(pagerange_idx, base_page=True)
+
+                                # update page we are working with
+                                pagerange_idx = len(self.table.pagerange) - 1
+                                pagerange = self.table.pagerange[pagerange_idx]
+                                use_page_idx = pagerange.base_page_idxs[-1]
+
+                            for i in range(self.num_columns):
+                                col_idx = i + 4
+
+                                # get offset index
+                                byte_offset = pagerange.page_to_num_records[page_idx] * pagerange.data_size
+                                data_size = pagerange.data_size
+
+                                # set data
+                                pagerange.array[col_idx][use_page_idx][byte_offset: byte_offset + 8] = self.pagerange[ranges][col_idx][page_idx_latest][byte_offset_latest]
+
+                            schema_encoding = "0" * self.table.num_columns
+                            page_idx_latest = int.from_bytes(pagerange.array[INDIRECTION_COLUMN][pages][offset: offset + 4], 'big')
+                            byte_offset_latest = int.from_bytes(pagerange.array[INDIRECTION_COLUMN][pages][offset + 4: offset + 8], 'big')
+
+                            pagerange.array[RID_COLUMN][page_idx][offset: offset + 8] = pagerange[RID_COLUMN][page][values]
+                            pagerange.array[TIMESTAMP_COLUMN][page_idx][offset: offset + 8] = int(datetime.now().timestamp()).to_bytes(8, 'big')
+                            pagerange.array[SCHEMA_ENCODING_COLUMN][page_idx][offset: offset + 8] = schema_encoding.to_bytes(8, 'big')
+
+                            pageidx = len(self.pagerange[ranges][INDIRECTION_COLUMN][byte_offset_latest])
+                            pagerange.array[INDIRECTION_COLUMN][page_idx][offset: offset + 4] = page.to_bytes(4, 'big')
+                            pagerange.array[INDIRECTION_COLUMN][page_idx][offset + 4: offset + 8] = byte_offset.to_bytes(4, 'big')
  
